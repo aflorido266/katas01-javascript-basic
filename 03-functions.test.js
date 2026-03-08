0187d314f37a63f8d1f285852e88faa06132090f
Ada <aflorido266@gmail.com>
2026-03-08 11:36:20 +0100

    Resuelto ejercicio 03-functions.test

--- 03-functions.test.js
+++ 03-functions.test.js
@@ -9,7 +9,9 @@ describe("Funciones en JavaScript", () => {
         let name = "Rosa";
 
         // Escribe aquí tu funcion "saludar"
-
+        function saludar (name){
+            return `Hola, ${name}`
+        }
 
         assert.strictEqual(saludar(name),"Hola, Rosa");
     })
@@ -20,6 +22,9 @@ describe("Funciones en JavaScript", () => {
         let num2 = 10;
 
         // Escribe aquí tu función "suma"
+        function suma(){
+            return num1+num2
+        }
 
 
         assert.strictEqual(suma(num1, num2),27);
@@ -34,8 +39,11 @@ describe("Funciones en JavaScript", () => {
         let rainProbability = "10%";
 
         // Escribe aquí tu función "weatherReport" (fíjate en la aserción para saber qué debe retornar esta función).
-
-        assert.strictEqual(weatherReport(userName, name, city, weather, rainProbability)).toEqual("Hola Ada Martín (ada89), hoy en Las Vegas el tiempo es soleado y las probabilidades de lluvia son del 10%.");
+        function weatherReport(userName, name, city, weather, rainProbability) {
+  
+        return `Hola ${name} (${userName}), hoy en ${city} el tiempo es ${weather} y las probabilidades de lluvia son del ${rainProbability}.`;
+}
+        assert.strictEqual(weatherReport(userName, name, city, weather, rainProbability), "Hola Ada Martín (ada89), hoy en Las Vegas el tiempo es soleado y las probabilidades de lluvia son del 10%.");
     })
 
 
@@ -44,16 +52,18 @@ describe("Funciones en JavaScript", () => {
     // Recuerda quitar la puntuación de comentario (/* ... */) para que este código a continuación sea legible, e inclúyelo dentro de tu test.
 
 
-     /*    let userName = "Ariana28";
+       it("BONUS: Crea un test que te permita testear la función", () => {
+        let userName = "Ariana28";
         let userAge = 38;
         let userEmail = "ariana_28@gmail.com";
         let userLocation = "Barcelona"
         let userIsRegistered = true;
         let password = "p4s$w0rDs3guR0";
 
-        function userAuth(userName, userEmail, password) {
-            return "Welcome ${userName}, your email is ${userEmail} and your password is ${password}."
-        } */
-
+       function userAuth(userName, userEmail, password) {
+          return `Welcome ${userName}, your email is ${userEmail} and your password is ${password}.`
+        } 
+assert.strictEqual(userAuth(userName, userEmail, password), "Welcome Ariana28, your email is ariana_28@gmail.com and your password is p4s$w0rDs3guR0.");
 
     })
+})
